bf88aac44e9d5583a04c33f8334f3973cd19be84
Pradish Bijukchhe <pradishbijukchhe@gmail.com>
2018-02-22 18:03:18 +0545

    Creates voter class

---
+++ src/main/java/gces/Vote.java
@@ -0,0 +1,22 @@
+package gces;
+
+public class Vote {
+    private String userKey;
+    private String voterKey;
+
+    public String getUserKey() {
+        return userKey;
+    }
+
+    public void setUserKey(String userKey) {
+        this.userKey = userKey;
+    }
+
+    public String getVoterKey() {
+        return voterKey;
+    }
+
+    public void setVoterKey(String voterKey) {
+        this.voterKey = voterKey;
+    }
+}
