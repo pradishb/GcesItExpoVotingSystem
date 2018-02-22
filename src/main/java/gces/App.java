276608648dd612b6bae102bebca3cd7f18b772d3
Pradish Bijukchhe <pradishbijukchhe@gmail.com>
2018-02-22 22:35:12 +0545

    Adds barcode reader functionality

--- src/main/java/gces/App.java
+++ src/main/java/gces/App.java
@@ -1,6 +1,10 @@
 package gces;
 
+import com.google.firebase.database.DatabaseReference;
 import javafx.application.Application;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
@@ -8,27 +12,59 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
 
-public class App extends Application {
+public class App extends Application implements BarcodeListener{
+    private Text text;
+    private Button btn;
+
     public static void main(String[] args) {
+        BarcodeReader x = new BarcodeReader();
+        FirebaseEngine.initialize();
         launch(args);
     }
 
     @Override
     public void start(Stage primaryStage) {
         primaryStage.setTitle("Hello World!");
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
 
+        text = new Text();
+
+        //Setting font to the text
+        text.setFont(new Font(45));
+
+        //setting the position of the text
+        text.setX(50);
+        text.setY(150);
+
+        //Setting the text to be added.
+        text.setText("Scan the barcode...");
+
+
+        btn = new Button();
+        btn.setText("Vote");
+        btn.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
+                btn.setVisible(false);
+//                Vote myVote = new Vote("xx", "yy");
+//                DatabaseReference ref = FirebaseEngine.database.getReference("votes");
+//                ref.push().setValueAsync(myVote);
             }
         });
 
         StackPane root = new StackPane();
+        root.getChildren().add(text);
         root.getChildren().add(btn);
-        primaryStage.setScene(new Scene(root, 300, 250));
+        btn.setVisible(false);
+        Scene scene = new Scene(root, 700, 600);
+        BarcodeReader.addReader(this, scene);
+        primaryStage.setScene(scene);
         primaryStage.show();
+    }
+
+    @Override
+    public void onBarcodeRead(String barcode) {
+        System.out.println("hello barcode is read : " + barcode);
+        text.setVisible(false);
+        btn.setVisible(true);
     }
 }
--- src/main/java/gces/BarcodeReader.java
+++ src/main/java/gces/BarcodeReader.java
@@ -1,63 +1,40 @@
 package gces;
 
-import java.awt.*;
-import java.awt.event.*;
-import java.util.List;
-import java.util.concurrent.CopyOnWriteArrayList;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.input.KeyCode;
 
 interface BarcodeListener{
     void onBarcodeRead(String barcode);
 }
 
-
 public class BarcodeReader{
-    private static final long THRESHOLD = 100;
+    private static final long THRESHOLD = 10;
     private static final int MIN_BARCODE_LENGTH = 8;
 
-    private final StringBuffer barcode = new StringBuffer();
-    private final List<BarcodeListener> listeners = new CopyOnWriteArrayList<>();
-    private long lastEventTimeStamp = 0L;
+    private static final StringBuffer barcode = new StringBuffer();
+    private static long lastTime;
 
-    public BarcodeReader() {
-        KeyboardFocusManager.getCurrentKeyboardFocusManager().addKeyEventDispatcher(new KeyEventDispatcher() {
-            @Override
-            public boolean dispatchKeyEvent(KeyEvent e){
-                if (e.getID() != KeyEvent.KEY_RELEASED) {
-                    return false;
-                }
-
-                if (e.getWhen() - lastEventTimeStamp > THRESHOLD) {
+    public static void addReader(BarcodeListener listener, Scene scene) {
+        lastTime = System.currentTimeMillis();
+        scene.setOnKeyReleased((new EventHandler<javafx.scene.input.KeyEvent>() {
+            public void handle(javafx.scene.input.KeyEvent event) {
+                long diff = System.currentTimeMillis() - lastTime;
+                lastTime = System.currentTimeMillis();
+                if (diff > THRESHOLD) {
                     barcode.delete(0, barcode.length());
                 }
 
-                lastEventTimeStamp = e.getWhen();
-
-                if (e.getKeyCode() == KeyEvent.VK_ENTER) {
+                if (event.getCode().equals(KeyCode.ENTER)){
                     if (barcode.length() >= MIN_BARCODE_LENGTH) {
-                        fireBarcode(barcode.toString());
+                        listener.onBarcodeRead(barcode.toString());
                     }
                     barcode.delete(0, barcode.length());
-                } else {
-                    barcode.append(e.getKeyChar());
                 }
-                return false;
+                else {
+                    barcode.append(event.getText());
+                }
             }
-        });
-
+        }));
     }
-
-    protected void fireBarcode(String barcode) {
-        for (BarcodeListener listener : listeners) {
-            listener.onBarcodeRead(barcode);
-        }
-    }
-
-    public void addBarcodeListener(BarcodeListener listener) {
-        listeners.add(listener);
-    }
-
-    public void removeBarcodeListener(BarcodeListener listener) {
-        listeners.remove(listener);
-    }
-
 }
